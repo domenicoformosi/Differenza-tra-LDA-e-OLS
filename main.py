494be697543f44dc112a90f2527545b4bccf016b
Domenico <147592835+domenicoformosi@users.noreply.github.com>
2025-01-18 13:01:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 # Common setup
 np.random.seed(42)
 n_samples = 100
+outliers1 = 95 ## values that you need to modify to change outliers proportion
+outliers2 = 80
 X_raw = np.random.normal(size=n_samples)
 X = sm.add_constant(X_raw)  # Add intercept
 
@@ -30,18 +32,18 @@ def run_regressions_and_plot(Y, ax, title_suffix):
 
 # Scenario 1: Outliers at Y[95:]
 Y1 = 2 * X[:, 1] + np.random.normal(size=n_samples)
-Y1[95:] += 20  # Introduce outliers in the last 5 points
+Y1[outliers1:] += 20  # Introduce outliers in the last 5 points
 
-# Scenario 2: Outliers at Y[80:]
+# Scenario 2: Outliers at Y[70:]
 Y2 = 2 * X[:, 1] + np.random.normal(size=n_samples)
-Y2[80:] += 20  # Introduce outliers in the last 30 points
+Y2[outliers2:] += 20  # Introduce outliers in the last 30 points
 
 # Create subplots (1 row, 2 columns)
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
 
 # Run regressions and plot on both subplots
-run_regressions_and_plot(Y1, ax1, "Outliers from index 95 to end")
-run_regressions_and_plot(Y2, ax2, "Outliers from index 80 to end")
+run_regressions_and_plot(Y1, ax1, "Outliers from index "+ str(outliers1) +" to end")
+run_regressions_and_plot(Y2, ax2, "Outliers from index "+str(outliers2)+ " to end")
 
 # Show the plots
 plt.tight_layout()
